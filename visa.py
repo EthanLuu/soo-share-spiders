b1e99a4368da3d6ed52d2c0f243ccde458cc24f9
EthanLuu <ethanlooo@outlook.com>
2023-08-08 19:31:57 +0800

    feat: update visa spider

--- visa.py
+++ visa.py
@@ -2,11 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from msedge.selenium_tools import Edge, EdgeOptions
 import time
 # 引入 ActionChains 类
 from selenium.webdriver.common.action_chains import ActionChains
 from config import Config
+from datetime import datetime
 import requests
 
 # options = EdgeOptions()
@@ -16,64 +16,95 @@ import requests
 #     options=options,
 #     executable_path=r"C:\Projects\lib\msedgedriver.exe")  # 相应的浏览器的驱动位置
 
-driver = webdriver.Chrome(executable_path=Config.chrome_path)
+from selenium.webdriver.chrome.options import Options
 
-# 登录
-driver.get("https://visa.vfsglobal.com/chn/zh/che/login")
+def check():
+    try:
+        opt = Options()
+        opt.binary_location = Config.chrome_path  # 指定chrome路径
+        # driver = webdriver.Chrome(options=opt)  # 创建Chrome对象时传入参数
+        driver = webdriver.ChromiumEdge()  # 创建Chrome对象时传入参数
+        driver.maximize_window()
+        
+        def wait_load():
+            WebDriverWait(driver, 20, 0.5).until_not(EC.visibility_of_element_located((By.TAG_NAME, "ngx-ui-loader")))
 
-time.sleep(5)
+        # 登录
+        driver.get("https://visa.vfsglobal.com/chn/zh/che/login")
 
-# 等待登录框出现
-element = WebDriverWait(driver, 5, 0.5).until(
-    EC.presence_of_element_located((By.ID, "mat-input-0")))
-driver.find_element_by_id("mat-input-0").send_keys(Config.vfs_user_name)
-password_input = driver.find_element_by_id("mat-input-1").send_keys(
-    Config.vfs_user_name)
+        time.sleep(5)
+        # 等待登录框出现
+        element = WebDriverWait(driver, 20, 0.5).until(
+            EC.presence_of_element_located((By.ID, "mat-input-0")))
+        driver.find_element(By.ID, "mat-input-0").send_keys(Config.vfs_user_name)
+        driver.find_element(By.ID, "mat-input-1").send_keys(Config.vfs_password)
 
-button = driver.find_element_by_class_name("btn-brand-orange")
-button.click()
+        button = driver.find_element(By.CLASS_NAME, "btn-brand-orange")
+        button.click()
+        
+        actions = ActionChains(driver)
 
-# 等待预约管理出现
-element = WebDriverWait(driver, 20, 0.5).until(
-    EC.presence_of_element_located(
-        (By.CSS_SELECTOR,
-         ".c-brand-orange.text-decoration-underline.cursor-pointer")))
-element.click()
+        while True:
+            # 点击预约管理
+            time.sleep(3)
+            element = WebDriverWait(driver, 20, 0.5).until(
+                EC.presence_of_element_located(
+                    (By.CSS_SELECTOR,
+                    ".c-brand-orange.text-decoration-underline.cursor-pointer")))
+            element.click()
 
-time.sleep(5)
-# 打开预约详情
-element = WebDriverWait(driver, 20, 0.5).until(
-    EC.presence_of_element_located((By.ID, "mat-expansion-panel-header-1")))
-actions = ActionChains(driver)
-actions.move_to_element(element).click().perform()
+            time.sleep(5)
+            # 展开预约详情
+            element = WebDriverWait(driver, 20, 0.5).until(
+                EC.presence_of_element_located((By.ID, "mat-expansion-panel-header-1")))
+            actions.move_to_element(element).click().perform()
 
-# 重新预约
-time.sleep(5)
-element = WebDriverWait(driver, 20, 0.5).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, ".fa-clock-o")))
-actions = ActionChains(driver)
-actions.move_to_element(element).click().perform()
+            # 重新预约
+            time.sleep(3)
+            element = WebDriverWait(driver, 20, 0.5).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, ".fa-clock-o")))
+            actions.move_to_element(element).click().perform()
+            
+            # 查看周五
+            time.sleep(8)
+            element = WebDriverWait(driver, 20, 0.5).until(
+                EC.presence_of_element_located(
+                    (By.CSS_SELECTOR, '[data-date="2023-08-11"]')))
+            class_name = element.get_attribute("class")
+            
+            title = "签证预约可用"
+            if class_name.find("date-availiable") >= 0:
+                print('Wechat sent')
+                requests.get(Config.server_url + "title=" + title)
+            else:
+                print("Friday Not Available")
 
-# 看看周五
-element = WebDriverWait(driver, 20, 0.5).until(
-    EC.presence_of_element_located(
-        (By.CSS_SELECTOR, '[data-date="2023-08-11"]')))
-class_name = element.get_attribute("class")
-print(class_name)
+            # 看看可预约日期
+            valids = []
+            for i in range(10, 30):
+                element = driver.find_element(By.CSS_SELECTOR, f'[data-date="2023-08-{str(i)}"]')
+                class_name = element.get_attribute("class")
+                if (class_name.find("date-availiable") >= 0):
+                    valids.append(i)
+            
+            
+            cur = datetime.now()
+            formatted_time = cur.strftime("%Y-%m-%d %H:%M:%S")
+            print(f"{formatted_time} 有效日期：{str(valids)}")
 
-# 下一页
-time.sleep(5)
-button = driver.find_element_by_css_selector(".fc-next-button")
-actions = ActionChains(driver)
-actions.move_to_element(element).click().perform()
+                
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            
+            break
+            # 返回个人中心
+            button = driver.find_element(By.CLASS_NAME, "btn-outline-brand-orange")
+            actions.move_to_element(button).click().perform()
+    except:
+        driver.quit()
 
-# 找到有效日期
-element = WebDriverWait(driver, 20, 0.5).until(
-    EC.presence_of_element_located(
-        (By.CSS_SELECTOR, '[data-date="2023-09-15"]')))
-class_name = element.get_attribute("class")
-print(class_name)
 
-title = "签证空闲日期提醒"
-content = "8/11"
-requests.get(Config.push_plus_url + "&title=" + title + "&content=" + content)
+while True:
+    try:
+        check()
+    except:
+        time.sleep(60)
